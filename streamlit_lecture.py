c5c1b7f23f8b7e455ef8caaec0ac58d6903ca974
shivamkumar12345 <sk9220449@gmail.com>
2024-05-05 16:40:36 +0530

    updated path

--- streamlit_lecture.py
+++ streamlit_lecture.py
@@ -5,16 +5,16 @@ import os
 import datetime
 st.title("My Dashboard")
 
-file = st.file_uploader("Upload file",type=({"csv","xlsx"}))
+# file = st.file_uploader("Upload file",type=({"csv","xlsx"}))
 
-dir = os.chdir(r"C:\Users\DELL\Desktop")
-if file is not None:
-    st.write("Selected file Name: " + file.name)
-    df= pd.read_csv(file.name,encoding="ISO-8859-1")
-    st.write(df)
-else:
-    df= pd.read_csv(r"C:\Users\DELL\Desktop\Superstore.csv",encoding="ISO-8859-1")
-    st.write(df)
+# dir = os.chdir(r"C:\Users\DELL\Desktop")
+# if file is not None:
+#     st.write("Selected file Name: " + file.name)
+#     df= pd.read_csv(file.name,encoding="ISO-8859-1")
+#     st.write(df)
+# else:
+df= pd.read_csv(r"https://github.com/shivamkumar12345/ecommerce_analysis/blob/main/Superstore.csv",encoding="ISO-8859-1")
+st.write(df)
 
 # col1,col2 = st.columns(2)
 
